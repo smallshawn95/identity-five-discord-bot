dbf7d35a1f7b9d41708b3f8bdac1749a84d1a8ef
Mango Jelly Pudding <74277414+EvanHsieh0415@users.noreply.github.com>
2024-01-29 14:34:55 +0000

    pep8

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
-import os, discord, asyncio
+import os
+import discord
+import asyncio
+
 from dotenv import load_dotenv
 from discord.ext import commands
 
+
 load_dotenv()
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix = commands.when_mentioned_or("!"), intents = intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -12,15 +17,18 @@ async def on_ready():
     print(f"{bot.user} is online")
     print(f"Synced {len(synced)} slash commands.")
 
+
 async def load_extensions():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
 
+
 async def main():
     async with bot:
         await load_extensions()
         await bot.start(os.getenv("DISCORD_BOT_TOKEN"))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
