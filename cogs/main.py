cdfdbf955f180465074ecb89242deda0951647dc
smallshawn95 <smallshawn95@gmail.com>
2024-01-29 17:07:31 +0800

    新增 main.py 檔案和 ping 功能

---
+++ cogs/main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class Main(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name = "ping")
+    async def ping_prefix(self, ctx: commands.Context):
+        await ctx.send(f"{round(self.bot.latency * 1000)}(ms)")
+
+    @app_commands.command(name = "ping")
+    async def ping_slash(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"{round(self.bot.latency * 1000)}(ms)")
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Main(bot))
